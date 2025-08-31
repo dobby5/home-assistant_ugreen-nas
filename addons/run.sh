8b186df30250d36d390423bce250e2f2684a5e34
Tom Bombadil <20954551+Tom-Bom-badil@users.noreply.github.com>
2025-08-31 17:47:23 +0200

    add time zone, central logging and log level

--- addons/run.sh
+++ addons/run.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
 set -e
+exec 2>&1
 
 CONFIG_FILE="/app/config.json"
+LOG_LEVEL=${LOG_LEVEL:-info}
+export TZ="${TZ:-Europe/Berlin}"
 
 if [[ -z "$UGREEN_NAS_API_SCHEME" ]]; then
-  echo "config.json"
-  export UGREEN_NAS_API_SCHEME=$(jq -r '.options.UGREEN_NAS_API_SCHEME' "$CONFIG_FILE")
-  export UGREEN_NAS_API_IP=$(jq -r '.options.UGREEN_NAS_API_IP' "$CONFIG_FILE")
-  export UGREEN_NAS_API_PORT=$(jq -r '.options.UGREEN_NAS_API_PORT' "$CONFIG_FILE")
-  export UGREEN_NAS_API_VERIFY_SSL=$(jq -r '.options.UGREEN_NAS_API_VERIFY_SSL' "$CONFIG_FILE")
+  printf "%s | INFO | --- Starting up with config.json. ---\n" "$(date +'%m.%d %H:%M:%S')"
+  export UGREEN_NAS_API_SCHEME="$(jq -r '.options.UGREEN_NAS_API_SCHEME' "$CONFIG_FILE")"
+  export UGREEN_NAS_API_IP="$(jq -r '.options.UGREEN_NAS_API_IP' "$CONFIG_FILE")"
+  export UGREEN_NAS_API_PORT="$(jq -r '.options.UGREEN_NAS_API_PORT' "$CONFIG_FILE")"
+  export UGREEN_NAS_API_VERIFY_SSL="$(jq -r '.options.UGREEN_NAS_API_VERIFY_SSL' "$CONFIG_FILE")"
 else
-  echo "docker-compose"
+  printf "%s | INFO | --- Starting up with docker-compose. ---\n" "$(date +'%m.%d %H:%M:%S')"
 fi
 
-echo "UGREEN_NAS_API_SCHEME: $UGREEN_NAS_API_SCHEME"
-echo "UGREEN_NAS_API_IP: $UGREEN_NAS_API_IP"
-echo "UGREEN_NAS_API_PORT: $UGREEN_NAS_API_PORT"
-echo "UGREEN_NAS_API_VERIFY_SSL: $UGREEN_NAS_API_VERIFY_SSL"
+printf "%s | INFO | Settings: %s://%s:%s verify_ssl=%s\n" \
+  "$(date +'%m.%d %H:%M:%S')" \
+  "${UGREEN_NAS_API_SCHEME}" \
+  "${UGREEN_NAS_API_IP:-<auto>}" \
+  "${UGREEN_NAS_API_PORT}" \
+  "${UGREEN_NAS_API_VERIFY_SSL}"
 
-exec uvicorn main:app --host 0.0.0.0 --port 4115
+exec uvicorn main:app \
+  --host 0.0.0.0 \
+  --port 4115 \
+  --log-config /app/logging.yaml \
+  --log-level $LOG_LEVEL
