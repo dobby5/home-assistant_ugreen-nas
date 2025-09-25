f5e80bed8722888f6a809c6ed05e2b2c59503c7e
Tom Bombadil <20954551+Tom-Bom-badil@users.noreply.github.com>
2025-09-25 20:34:14 +0200

    Remove proxy port, remove ssl_verify

--- custom_components/ugreen/const.py
+++ custom_components/ugreen/const.py
@@ -1,11 +1,9 @@
 DOMAIN = "ugreen"
 PLATFORMS = ["sensor", "button"]
 
-CONF_UGREEN_HOST = "ugreen_host"
-CONF_UGREEN_PORT = "ugreen_port"
-CONF_AUTH_PORT = "auth_port"
-CONF_USE_HTTPS = "use_https"
-CONF_VERIFY_SSL = "verify_ssl"
-
 CONF_USERNAME = "username"
 CONF_PASSWORD = "password"
+
+CONF_UGREEN_HOST = "ugreen_host"
+CONF_UGREEN_PORT = "ugreen_port"
+CONF_USE_HTTPS = "use_https"
